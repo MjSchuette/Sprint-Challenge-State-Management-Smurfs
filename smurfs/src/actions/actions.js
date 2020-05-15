95bb64cd09fb1b4b31551d431880a2420f467813
MatthewSchuette <mjschuette4@gmail.com>
2020-05-15 10:54:44 -0500

    Added reducer cases

--- smurfs/src/actions/actions.js
+++ smurfs/src/actions/actions.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const GET_DATA = 'GET_DATA';
 export const LOADING_SUCCESS = 'LOADING-SUCCESS';
 
+//Loads smurfs from server to screen
 export const loadSmurf = () => dispatch => {
     dispatch({ type: GET_DATA });
     axios
@@ -11,8 +12,9 @@ export const loadSmurf = () => dispatch => {
             dispatch({ type: LOADING_SUCCESS, payload: res.data });
         })
 }
+
 //adds new smurf to the thing
-export const addSmurf = () => dispatch => {
+export const addSmurf = smurf => dispatch => {
     axios
         .post('http://localhost:3333/smurfs', smurf)
         .then(res => {
