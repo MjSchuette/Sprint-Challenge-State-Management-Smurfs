1cc7ab6b161005ca8c778c99d38fa83f88fcfc7c
MatthewSchuette <mjschuette4@gmail.com>
2020-05-15 11:02:05 -0500

    Can now add smurfs

--- smurfs/src/components/Card.js
+++ smurfs/src/components/Card.js
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
 import { Button, Input, Form } from 'reactstrap';
+import { addSmurf } from '../actions/actions';
+import { useDispatch } from 'react-redux';
 
 const Card = () => {
-const [data, setData] = useState({
-    name: '',
-    age: '',
-    height: '',
-    id: Date.now()
-})
-    const handleChanges = e => {
-        e.preventDefault()
-        setData({ ...data, [e.target.name]: e.target.value })
-    }
-    return (
-        <Form className='Card'>
-            <Input type='text' name='name' placeholder='Smurf Name' onChange={handleChanges} />
-            <Input type='text' name='age' placeholder='Smurf Age' onChange={handleChanges} />
-            <Input type ='text' name='height' placeholder='Smurf Height' onChange={handleChanges} />
-            <Button>Add Smurf</Button>
-        </Form>
-    );
+    const dispatch = useDispatch();
+    const [data, setData] = useState({
+        name: '',
+        age: '',
+        height: '',
+        id: Date.now()
+    });
+        const handleChanges = e => {
+            e.preventDefault()
+            setData({ ...data, [e.target.name]: e.target.value })
+        };
+
+        return (
+            <Form 
+                className='Card'
+                onSubmit={e => {
+                    e.preventDefault();
+                    dispatch(addSmurf(data));
+                    setData({ name: '', age: '', height: '', id:Date.now() });
+                }}>
+                <Input type='text' name='name' placeholder='Smurf Name' onChange={handleChanges} /><br />
+                <Input type='text' name='age' placeholder='Smurf Age' onChange={handleChanges} /><br />
+                <Input type ='text' name='height' placeholder='Smurf Height' onChange={handleChanges} /><br />
+                <Button>Add Smurf</Button>
+            </Form>
+        );
 };
 
 export default Card;
